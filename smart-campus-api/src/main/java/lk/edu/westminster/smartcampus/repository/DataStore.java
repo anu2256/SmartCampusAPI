e1c6c9f0b50015abb66ea5e00a426c2f23c7306e
anu2256 <anuththarajayarathne22@gmail.com>
2026-04-21 13:47:10 +0530

    create SensorResource.java file
    create resource package and fix errors in DataStore.java file

--- smart-campus-api/src/main/java/lk/edu/westminster/smartcampus/repository/DataStore.java
+++ smart-campus-api/src/main/java/lk/edu/westminster/smartcampus/repository/DataStore.java
@@ -1,12 +1,16 @@
 package lk.edu.westminster.smartcampus.repository;
 
 import lk.edu.westminster.smartcampus.model.Room;
+import lk.edu.westminster.smartcampus.model.Sensor;
+import lk.edu.westminster.smartcampus.model.SensorReading;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DataStore {
     private static DataStore instance;
     private List<Room> rooms = new ArrayList<>();
+    private List<Sensor> sensors = new ArrayList<>(); // අලුතින් එකතු කළා
+    private List<SensorReading> readings = new ArrayList<>(); // අලුතින් එකතු කළා
 
     private DataStore() {}
 
@@ -18,4 +22,6 @@ public class DataStore {
     }
 
     public List<Room> getRooms() { return rooms; }
+    public List<Sensor> getSensors() { return sensors; } // Getter එක
+    public List<SensorReading> getReadings() { return readings; } // Getter එක
 }
---
+++ smart-campus-api/src/main/java/lk/edu/westminster/smartcampus/resource/SensorResource.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lk.edu.westminster.smartcampus.resource;
+
+import lk.edu.westminster.smartcampus.model.Sensor;
+import lk.edu.westminster.smartcampus.repository.DataStore;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+/**
+ *
+ * @author USER
+ */
+@Path("/sensors")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class SensorResource {
+
+    @GET
+    public List<Sensor> getAllSensors() {
+        // Singleton එකෙන් දත්ත ගන්න විදිය
+        return DataStore.getInstance().getSensors();
+    }
+
+    @POST
+    public Sensor addSensor(Sensor sensor) {
+        // Singleton එකට දත්ත දාන විදිය
+        DataStore.getInstance().getSensors().add(sensor);
+        return sensor;
+    }
+}
