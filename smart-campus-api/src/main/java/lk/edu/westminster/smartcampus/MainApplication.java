0a693386210c93e97bd5fd97020b59e5c1e83769
anu2256 <anuththarajayarathne22@gmail.com>
2026-04-22 15:51:28 +0530

    Add jakarta.ws.rs to mainapplication.java

--- smart-campus-api/src/main/java/lk/edu/westminster/smartcampus/MainApplication.java
+++ smart-campus-api/src/main/java/lk/edu/westminster/smartcampus/MainApplication.java
@@ -6,8 +6,8 @@ package lk.edu.westminster.smartcampus;
 
 import lk.edu.westminster.smartcampus.resource.RoomResource;
 import lk.edu.westminster.smartcampus.resource.SensorResource;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
 import java.util.HashSet;
 import java.util.Set;
 
