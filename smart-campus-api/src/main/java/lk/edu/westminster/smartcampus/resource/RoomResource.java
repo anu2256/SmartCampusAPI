a700aa76ab1e72c8f6c63f40716da49357c72978
anu2256 <anuththarajayarathne22@gmail.com>
2026-04-22 15:00:08 +0530

    update roomresoure.java

--- smart-campus-api/src/main/java/lk/edu/westminster/smartcampus/resource/RoomResource.java
+++ smart-campus-api/src/main/java/lk/edu/westminster/smartcampus/resource/RoomResource.java
@@ -4,10 +4,39 @@
  */
 package lk.edu.westminster.smartcampus.resource;
 
-/**
- *
- * @author USER
- */
+import lk.edu.westminster.smartcampus.model.Room;
+import lk.edu.westminster.smartcampus.repository.DataStore;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/rooms")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class RoomResource {
-    
+
+    // GET /api/v1/rooms - සියලුම කාමර ලැයිස්තුව ලබා ගැනීමට
+    @GET
+    public List<Room> getAllRooms() {
+        return DataStore.getInstance().getRooms();
+    }
+
+    // POST /api/v1/rooms - නව කාමරයක් නිර්මාණය කිරීමට
+    @POST
+    public Room addRoom(Room room) {
+        DataStore.getInstance().getRooms().add(room);
+        return room;
+    }
+
+    // GET /api/v1/rooms/{roomId} - නිශ්චිත කාමරයක් සොයා ගැනීමට
+    @GET
+    @Path("/{roomId}")
+    public Room getRoom(@PathParam("roomId") String roomId) {
+        for (Room r : DataStore.getInstance().getRooms()) {
+            if (r.getId().equals(roomId)) {
+                return r;
+            }
+        }
+        throw new NotFoundException("Room with ID " + roomId + " not found.");
+    }
 }
