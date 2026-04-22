c3c277753b8692a681b68e6531051a64f7bf6956
anu2256 <anuththarajayarathne22@gmail.com>
2026-04-22 23:44:07 +0530

    update senserreadingresours.java

--- smart-campus-api/src/main/java/lk/edu/westminster/smartcampus/resource/SensorReadingResource.java
+++ smart-campus-api/src/main/java/lk/edu/westminster/smartcampus/resource/SensorReadingResource.java
@@ -4,11 +4,11 @@
  */
 package lk.edu.westminster.smartcampus.resource;
 
+import lk.edu.westminster.smartcampus.model.Sensor; // අනිවාර්යයෙන්ම මේ import එක එකතු කරන්න
 import lk.edu.westminster.smartcampus.model.SensorReading;
 import lk.edu.westminster.smartcampus.repository.DataStore;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,7 +22,6 @@ public class SensorReadingResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<SensorReading> getReadings() {
-        // එම sensorId එකට අදාළ readings පමණක් filter කර ලබා දේ
         return DataStore.getInstance().getReadings().stream()
                 .filter(r -> String.valueOf(r.getSensorId()).equals(sensorId))
                 .collect(Collectors.toList());
@@ -32,8 +31,22 @@ public class SensorReadingResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public SensorReading addReading(SensorReading reading) {
+        // 1. Reading එකට අදාළ Sensor ID එක දීම
         reading.setSensorId(sensorId);
+        
+        // 2. DataStore එකට Reading එක එකතු කිරීම
         DataStore.getInstance().getReadings().add(reading);
+        
+        // 3. Side Effect: අදාළ Sensor එකේ currentValue එක update කිරීම
+        Sensor parentSensor = DataStore.getInstance().getSensors().stream()
+                .filter(s -> String.valueOf(s.getId()).equals(sensorId))
+                .findFirst()
+                .orElse(null);
+        
+        if (parentSensor != null) {
+            parentSensor.setCurrentValue(reading.getValue());
+        }
+        
         return reading;
     }
 }
